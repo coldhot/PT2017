073580f0e676e148cc2ce20653427fb9f0e6c622
dauren <yesdauren@gmail.com>
2017-02-28 15:56:58 +0600

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -6,20 +6,38 @@ from telegram.ext import Updater
 updater = Updater(token='284691436:AAExrkDmO8RfmfyVYE4LVTNxPixnaUW6F3U')
 dispatcher = updater.dispatcher
 
-def start(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def easy_start(bot, update):
+	bot.sendMessage(chat_id=update.message.chat_id, text="Salem, Alem!")
 
 def salem(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Aleikum asalam!")
-	
+
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', easy_start)
+dispatcher.add_handler(start_handler)
+
+
 salem_handler = CommandHandler('salem', salem)
 dispatcher.add_handler(salem_handler)
 
+def weather(bot, update):
+	import requests
+	from bs4 import BeautifulSoup 
+	url = 'http://kazhydromet.kz/ru/almaty'
+	r = requests.get(url)
+	html = r.text
+	soup = BeautifulSoup(html, "html.parser")
+	wtext = soup.select(".w_map span")[0].text
+	bot.sendMessage(chat_id=update.message.chat_id, text=wtext)
+
+weather_handler = CommandHandler('weather', weather)
+dispatcher.add_handler(weather_handler)
+
 updater.start_polling()
+
+
+
 
 
 
