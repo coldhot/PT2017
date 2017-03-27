c7a9f9022ac77caf1d9468e3f5126ff4382d2826
Dauren Yesmukhanov <yesdauren@gmail.com>
2017-03-27 09:54:22 +0600

    update week09

--- week09/01.py
+++ week09/01.py
@@ -1,7 +1,7 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 text = "S"
 color = (0, 0, 120)
-img = Image.new('RGB', (10, 20), color)
+img = Image.new('RGB', (10, 10), color)
 imgDrawer = ImageDraw.Draw(img)
 imgDrawer.text((2, -2), text)
 img.save("pil-example-01.png")
